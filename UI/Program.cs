365910595324a41f15cbfe5814b75dede4d69a38
NhoxLeo <vvip159248@gmail.com>
2019-06-10 00:06:27 +0700

    Minor Updates

--- UI/Program.cs
+++ UI/Program.cs
@@ -16,7 +16,7 @@ namespace UI
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new PotentialStudentForm());
+            Application.Run(new MenuForm());
         }
     }
 }
